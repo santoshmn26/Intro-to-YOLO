dc866d07c3dab0313e69a1ddcaabe291ec382413
Santosh M N <31931121+santoshmn26@users.noreply.github.com>
2019-05-13 23:26:13 -0500

    Add files via upload

--- Custom-Object-Detection/main.py
+++ Custom-Object-Detection/main.py
@@ -1,60 +1,95 @@
 import cv2
 from darkflow.net.build import TFNet
 import numpy as np
-import os   
 import datetime
 import time
 import threading 
+import math
 
-option = {
-    'model': 'cfg/tiny-yolo-voc-3c.cfg',
-    'load': -1,
-    'threshold': 0.4
-}
+
+# Drawing bounding boxes
+def draw_border(img, pt1, pt2, color, thickness, r, d,i):           
+
+    x1,y1 = pt1
+    x2,y2 = pt2
+
+    # Top left
+    cv2.line(img, (x1 + r, y1), (x1 + r + d, y1), color, thickness)
+    cv2.line(img, (x1, y1 + r), (x1, y1 + r + d), color, thickness)
+    cv2.line(img, (x1 + r + d, y1), (x2 - r - d, y1),color, 1)                  # Thin line
+    cv2.ellipse(img, (x1 + r, y1 + r), (r, r), 180, 0, 90, color, thickness)
+
+    # Top right
+    cv2.line(img, (x2 - r, y1), (x2 - r - d, y1), color, thickness)
+    cv2.line(img, (x2, y1 + r), (x2, y1 + r + d), color, thickness)
+    cv2.line(img, (x2, y1 + r + d), (x2, y2 - r - d), color, 1)                 # Thin line
+    cv2.ellipse(img, (x2 - r, y1 + r), (r, r), 270, 0, 90, color, thickness)
+    #Bottom left
+
+    cv2.line(img, (x1 + r, y2), (x1 + r + d, y2), color, thickness)
+    cv2.line(img, (x1, y2 - r), (x1, y2 - r - d), color, thickness)
+    cv2.line(img, (x1 + r + d, y2), (x2 - r - d, y2), color, 1)                 # Thin line
+    cv2.ellipse(img, (x1 + r, y2 - r), (r, r), 90, 0, 90, color, thickness)
+    # Bottom right
+
+    cv2.line(img, (x2 - r, y2), (x2 - r - d, y2), color, thickness)
+    cv2.line(img, (x2, y2 - r), (x2, y2 - r - d), color, thickness)
+    cv2.line(img, (x1, y1 + r + d), (x1, y2 - r - d), color, 1)                 # Thin line
+    cv2.ellipse(img, (x2 - r, y2 - r), (r, r), 0, 0, 90, color, thickness)
+
+
+
+
 def write_data(result):
+
     f=open("log.txt",'a+')
     for i in results:
-        f.write(str(datetime.datetime.now())+','+i['label']+','+str(i['confidence'])+','+str(i['topleft']['x'])+','+str(i['topleft']['y'])+','+str(i['bottomright']['x'])+','+str(i['bottomright']['y'])+','+'camera\n')
+        f.write(str(datetime.datetime.now())+','+i['label']+','+str(i['confidence'])+','+str(i['topleft']['x'])+','+str(i['topleft']['y'])+','+str(i['bottomright']['x'])+','+str(i['bottomright']['y'])+','+video_source+'\n')
         f.close
 
-tfnet = TFNet(option)
-capture = cv2.VideoCapture(0)
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-colors = [tuple(255 * np.random.rand(3)) for i in range(20)]
-out=cv2.VideoWriter('output_videofile.mp4',fourcc, 20.0, (640,480))
+if __name__=="__main__":
 
 
-while (capture.isOpened()):
-    stime = time.time()
-    ret, frame = capture.read()
-    if ret:
-        thread_count = threading.enumerate()
-        results = tfnet.return_predict(frame)
-        if (len(thread_count)==12):
-            print(">>>>Updating logfile!")
-            threading.Timer(5.0, write_data,args=(results,)).start()    
-        for color, result in zip(colors, results):
-            #if(result['label']=='no_hat'):
-                print(result['label'],"\t",(result['confidence']))
-                #frame = cv2.rectangle(frame, (result['topleft']['x'], result['topleft']['y']), (result['bottomright']['x'], result['bottomright']['y']), color, 3)
-                
-                cv2.line(frame,(result['topleft']['x'], result['topleft']['y']),(result['topleft']['x']+15, result['topleft']['y']),(0,255,255),5)
-                cv2.line(frame,(result['topleft']['x'], result['topleft']['y']),(result['topleft']['x'], result['topleft']['y']+15),(0,255,255),5)
-                cv2.line(frame,(result['topleft']['x'], result['bottomright']['y']),(result['topleft']['x']+15, result['bottomright']['y']),(0,255,255),5)
-                cv2.line(frame,(result['topleft']['x'], result['bottomright']['y']),(result['topleft']['x'], result['bottomright']['y']-15),(0,255,255),5)
-                
-                cv2.line(frame,(result['bottomright']['x'], result['bottomright']['y']),(result['bottomright']['x']-15, result['bottomright']['y']),(0,255,255),5)
-                cv2.line(frame,(result['bottomright']['x'], result['bottomright']['y']),(result['bottomright']['x'], result['bottomright']['y']-15),(0,255,255),5)
-                cv2.line(frame,(result['bottomright']['x'], result['topleft']['y']),(result['bottomright']['x']-15, result['topleft']['y']),(0,255,255),5)
-                cv2.line(frame,(result['bottomright']['x'], result['topleft']['y']),(result['bottomright']['x'], result['topleft']['y']+15),(0,255,255),5)
-                
-                
-                frame = cv2.putText(frame, result['label'],((result['topleft']['x'])+6,(result['bottomright']['y'])-6), cv2.FONT_HERSHEY_DUPLEX, 1, (0,0,0), 1)
-        cv2.imshow('frame', frame)
-        out.write(frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+
+    option = {
+        'model': 'cfg/tiny-yolo-voc-3c.cfg',  # Model to load
+        'load': -1,                           # Weights to load
+        'threshold': 0.3,                     # Threshold value
+        'gpu': 0                              # GPU value (0 - 1)         
+        
+    }
+    video_source='camera'
+    
+
+    
+    tfnet = TFNet(option)
+    capture = cv2.VideoCapture(0)        # Place your path to your video file, or assign 0 to load webcam feed.
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+
+    initial_thread_count = threading.enumerate()
+    i,prev=1,(0,0)
+    while (capture.isOpened()):
+        stime = time.time()
+        ret, frame = capture.read()
+        if ret:
+            thread_count = threading.enumerate()
+            results = tfnet.return_predict(frame)
+            if (len(thread_count)==len(initial_thread_count)):
+                threading.Timer(5.0, write_data,args=(results,)).start()    
+            for result in results:
+
+                if(result['label']=='no_hat'):
+                    draw_border(frame, ((result['topleft']['x']),(result['topleft']['y'])), ((result['bottomright']['x']),(result['bottomright']['y'])), (0,0,255), 4, 0, 14,i)
+                    
+                #else:
+                    draw_border(frame, ((result['topleft']['x']),(result['topleft']['y'])), ((result['bottomright']['x']),(result['bottomright']['y'])), (0,0,255), 4, 5, 14,1)
+                frame = cv2.putText(frame, result['label'],((result['topleft']['x']),(result['bottomright']['y'])+30), cv2.FONT_HERSHEY_DUPLEX, 1, (0,0,0), 1)    
+            cv2.imshow('frame', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        else:
+            capture.release()
+            cv2.destroyAllWindows()
             break
-    else:
-        capture.release()
-        cv2.destroyAllWindows()
-        break
+
+#===============================================================================
